59c48578a5e46e28ff342de342545590fa018adc
Sabbirz <sabbir.kuasha@gmail.com>
2022-10-16 15:07:59 +0600

    qr code

---
+++ src/routes/qr/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import QrCode from 'sve'
+</script>
+
+<div class="container">
+	<h1>
+		Svelte Mocktail Island QR code
+	</h1>
+  <QrCode value="https://www.facebook.com/Mocktailisland" />
+</div>
